66f39ed1cf0f7ff2481644576f7f10cb1f887b7d
“skuralll” <f20154@e.hiroshima-cu.ac.jp>
2023-05-10 23:16:21 +0900

    コピーライトの追加

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,9 @@ import {
   ActionIcon,
   rem,
   Image,
+  Text,
 } from "@mantine/core";
+import Link from "@/components/link";
 
 const useStyles = createStyles((theme) => ({
   footer: {
@@ -15,31 +17,45 @@ const useStyles = createStyles((theme) => ({
     }`,
   },
 
+  // inner: {
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   paddingTop: theme.spacing.xl,
+  //   paddingBottom: theme.spacing.xl,
+
+  //   [theme.fn.smallerThan("xs")]: {
+  //     flexDirection: "column",
+  //   },
+  // },
+
   inner: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    flexDirection: "column",
     paddingTop: theme.spacing.xl,
     paddingBottom: theme.spacing.xl,
-
-    [theme.fn.smallerThan("xs")]: {
-      flexDirection: "column",
-    },
   },
 
-  links: {
-    [theme.fn.smallerThan("xs")]: {
-      marginTop: theme.spacing.md,
-    },
+  legends: {
+    marginBottom: rem(10),
   },
 }));
 
+// フッター
 export default function Footer() {
   const { classes } = useStyles();
 
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
+        <Container className={classes.legends}>
+          <Text fz="md" c="gray.7">
+            © 2023 skuralll
+          </Text>
+          {/* <Link href={"mailto:skuralll.com@gmail.com"} external></Link> */}
+        </Container>
         <Image
           src="https://moe-counter.yude.jp/get/@:skuralll"
           width={315}
