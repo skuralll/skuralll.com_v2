80ad8611490f90574b6e0dfb1673c4653a80edea
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-05-18 13:37:29 +0900

    不要なインポートの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import User from "@/components/profile/user";
-import { User as UserModel } from "@/models/user";
-import { user } from "@/config/user";
-import Socials from "@/components/profile/socials";
-import { Divider, Stack, createStyles } from "@mantine/core";
-import Certification from "@/components/profile/certifications";
+import { Stack } from "@mantine/core";
 import Contacts from "@/components/profile/contacts";
 
 export default function Home() {
