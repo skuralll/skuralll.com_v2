bb48d226389aff39345aa754b8aecc3b3c7874ee
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-05-18 09:29:25 +0900

    CSSの整理

--- src/components/profile/contacts.tsx
+++ src/components/profile/contacts.tsx
@@ -24,18 +24,7 @@ import Section from "../utils/section";
 import { IconAddressBook, IconShare } from "@tabler/icons-react";
 import Link from "../utils/link";
 
-const useStyles = createStyles((theme) => ({
-  about: {
-    // width: rem(512),
-    // textAlign: "left",
-  },
-
-  title: {
-    // borderLeft: `inset ${rem(5)}`,
-    // paddingLeft: rem(10),
-    // marginBottom: rem(20),
-  },
-}));
+const useStyles = createStyles((theme) => ({}));
 
 export default function Contacts() {
   const { classes, cx } = useStyles();
