4734754cc0985dd538e50269442a3c1e815a0c31
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-05-19 20:45:01 +0900

    フォントを変更

--- src/components/app/theme.ts
+++ src/components/app/theme.ts
@@ -2,15 +2,11 @@ import { AnchorProps, MantineThemeOverride, PaperProps } from "@mantine/core";
 
 export const theme: MantineThemeOverride = {
   colorScheme: "light",
-  fontFamily: "Open Sans",
+  fontFamily: "Noto Sans Japanese, Open Sans",
   globalStyles: (theme) => ({
     body: {
       backgroundColor: "#F8F9FA",
     },
-    // a: {
-    //   textDecoration: "none",
-    //   color: theme.black,
-    // },
   }),
   components: {},
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,12 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
