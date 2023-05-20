73bb85d733a1ac70dcd0082e2d0ee3e73b0aae4d
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-05-20 10:08:47 +0900

    メタデータを設定

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import { siteMeta as siteMetaModel } from "@/models/site";
+
+export const siteMeta: siteMetaModel = {
+  siteTitle: "skuralll.com",
+  siteDesc: "skuralllの個人サイトです。",
+  siteUrl: "https://skuralll.com",
+  siteLang: "ja",
+  siteLocale: "ja_JP",
+  siteType: "website",
+  siteIcon: "/favicon.png",
+};
---
+++ src/models/site.ts
@@ -0,0 +1,9 @@
+export type siteMeta = {
+  siteTitle: string;
+  siteDesc: string;
+  siteUrl: string;
+  siteLang: string;
+  siteLocale: string;
+  siteType: string;
+  siteIcon: string;
+};
