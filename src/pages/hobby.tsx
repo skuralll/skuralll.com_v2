7a6fd6c9e3f8f5ec2a009e7fa548dce7fd4936b0
“skuralll” <f20154@e.hiroshima-cu.ac.jp>
2023-06-10 17:26:51 +0900

    Steamの欄を追加

--- src/pages/hobby.tsx
+++ src/pages/hobby.tsx
@@ -1,3 +1,15 @@
+import Meta from "@/components/app/meta";
+import Game from "@/components/hobby/game";
+import { Center, Stack } from "@mantine/core";
+import Script from "next/script";
+
 export default function Hobby() {
-  return <></>;
+  return (
+    <>
+      <Meta pageTitle="skuralll.com | Hobby" pageDesc="趣味など" />
+      <Stack spacing="lg" mb="md">
+        <Game />
+      </Stack>
+    </>
+  );
 }
